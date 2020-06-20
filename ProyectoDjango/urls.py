fae5c54dafe5214752da7a6684ead3cdaf493343
Javier <mayorgas1996@correo.ugr.es>
2020-06-20 19:04:25 +0200

    Creación de vista inicio y sobre nosotros
    Se crean las vistas y se añaden al fichero de urls
    También se crear una carpeta para alojar los htmls llamada 'templates' y se subdividen las carpetas para cada página

--- ProyectoDjango/urls.py
+++ ProyectoDjango/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.index, name = "inicio"),
+    path('', views.index, name = "index"),
+    path('sobre-nosotros/', views.about, name = "about"),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'main_app/index.html', {
+        'title':'Inicio'
+    })
+
+def about(request):
+    return render(request, 'main_app/about.html', {
+        'title':'Sobre nosotros'
+    })
