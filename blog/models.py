117ce260572f18920e3fae2702d05bfa118c9cbf
Javier <mayorgas1996@correo.ugr.es>
2020-06-22 20:44:30 +0200

    Creación de la clase 'Category' dentro de la app 'blog'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    description = models.CharField(max_length=255, verbose_name='Descripción')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha creación")
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+    
+    def __str__(self):
+        return self.name
