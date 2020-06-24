ac2b971349b892952a3b96013f4619a8938dff34
Javier <mayorgas1996@correo.ugr.es>
2020-06-24 20:14:30 +0200

    Registro e Inicio de sesión
    Además se adapta la barra de navegación en función de si el usuario está logueado o no.

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from main_app.forms import RegisterForm
+from django.contrib.auth import authenticate,login, logout
 
 # Create your views here.
 
@@ -15,19 +18,39 @@ def about(request):
 
 def register_page(request):
 
-    register_form = UserCreationForm()
+    register_form = RegisterForm()
 
     if request.method == 'POST':
-        register_form = UserCreationForm(request.POST)
+        register_form = RegisterForm(request.POST)
 
         #Se guarda el usuario
         if register_form.is_valid():
             register_form.save()
-
+            messages.success(request, 'Te has registrado correctamente')
             return redirect('inicio')
 
 
     return render(request, 'users/register.html',{
         'title':'Registro',
         'register_form': register_form
+    })
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Has iniciado sesión correctamente')
+            return redirect('inicio')
+
+        else:
+            messages.warning(request, 'No te has identificado correctamente')
+
+    return render(request, 'users/login.html',{
+        'title':'Iniciar sesión'
     })
